4594596a9b7e3ad0b28a71c6f4979cc8d0db2f58
Brynfor Morris <brynfor.morris@gmail.com>
2023-02-17 07:24:59 -0600

    provided alternate solution to Palindrome

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
+function reverseString(word){
+  // TODO: implement string reversing functionality
+  const reversedWord = wordArray.split("").reverse().join("");
+  return reversedWord;
+}
+
 function isPalindrome(word) {
-  // Write your algorithm here
+  
+  // Reverse input string.
+  const reversedWord = reverseString(word)
+  // IF string reversed is same as input word, return true,
+  return word === reversedWord
+  // else
+  // return false
+
+
+
+  // Need to make isPalindrome function return either true or false.
+  // When input string is the same forwards and backwards, isPalindrome should return true.
+  // This means that if input string is the same after I reverse it, it should be true.
+
+  // IE: "mom" in reverse is also "mom", so my solution should return true. 
+  // IE: "hi" in reverse is "ih" so it should return false.
 }
 
 /* 
-  Add your pseudocode here
+  Reverse input string.
+
+  IF string reversed is same as input word, return true,
+  else
+  return false
 */
 
 /*
@@ -17,6 +42,12 @@ if (require.main === module) {
   console.log("=>", isPalindrome("racecar"));
 
   console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
